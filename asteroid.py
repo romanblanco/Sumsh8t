cd295d6fb4556d6f195ef9d23a68c4eb6746e000
Tomas2198 <58487631+Tomas2198@users.noreply.github.com>
2019-12-05 22:21:39 +0100

    Update asteroid.py

--- asteroid.py
+++ asteroid.py
@@ -13,8 +13,11 @@ mimoy = -9000
 mimolx = -10000
 mimoly = -10000
 
+Met = 0
+
 window = pyglet.window.Window(WIDTH, HEIGHT)
 
+pocet_meteoru = 1
 
 ACCELERATION = 150
 LASER_ACCELERATION = 2000
@@ -22,119 +25,89 @@ METEORB_ACCELERATION = 20
 METEORM_ACCELERATION = 40
 METEORS_ACCELERATION = 60
 METEORT_ACCELERATION = 80
-rotation_speed = 100
+ROTATION_SPEED = 100
 
 batch = pyglet.graphics.Batch()
 
-Metb1 = pyglet.image.load('meteorGrey_big4.png')
-Metb1.anchor_x = Metb1.width // 2
-Metb1.anchor_y = Metb1.height // 2
-
-Metm1 = pyglet.image.load('meteorGrey_med1.png')
-Metm1.anchor_x = Metm1.width // 2
-Metm1.anchor_y = Metm1.height // 2
-
-Metm2 = pyglet.image.load('meteorGrey_med2.png')
-Metm2.anchor_x = Metm2.width // 2
-Metm2.anchor_y = Metm2.height // 2
-
-Mets1 = pyglet.image.load('meteorGrey_small1.png')
-Mets1.anchor_x = Mets1.width // 2
-Mets1.anchor_y = Mets1.height // 2
-
-Mets2 = pyglet.image.load('meteorGrey_small2.png')
-Mets2.anchor_x = Mets2.width // 2
-Mets2.anchor_y = Mets2.height // 2
-
-Mett1 = pyglet.image.load('meteorGrey_tiny1.png')
-Mett1.anchor_x = Mett1.width // 2
-Mett1.anchor_y = Mett1.height // 2
-
-Mett2 = pyglet.image.load('meteorGrey_tiny2.png')
-Mett2.anchor_x = Mett2.width // 2
-Mett2.anchor_y = Mett2.height // 2
 
 stisknute_klavesy = set()
-meteory = set()
-meteory_2 = set()
-meteory_3 = set()
-meteory_4 = set()
-zivoty = set()
+
+
+meteory_b = []
+meteory_m = []
+meteory_s = []
+meteory_t = []
+
+meteory_b2 = []
+meteory_m2 = []
+meteory_s2 = []
+meteory_t2 = []
+
+meteory_b3 = []
+meteory_m3 = []
+meteory_s3 = []
+meteory_t3 = []
+
+meteory_big = []
+meteory_med = []
+meteory_small = []
+meteory_tiny = []
+
+meteory_big.append('meteorGrey_big1.png')
+meteory_big.append('meteorGrey_big2.png')
+meteory_big.append('meteorGrey_big3.png')
+meteory_big.append('meteorGrey_big4.png')
+
+meteory_med.append('meteorGrey_med1.png')
+meteory_med.append('meteorGrey_med2.png')
+
+meteory_small.append('meteorGrey_small1.png')
+meteory_small.append('meteorGrey_small2.png')
+
+meteory_tiny.append('meteorGrey_tiny1.png')
+meteory_tiny.append('meteorGrey_tiny2.png')
+
 pozice = [20, HEIGHT-20]
 
-class Meteor:
+class Object:
 
-    def __init__(self,met):
+    def __init__(self, vec, imgpng, acc, rot, ziv):
 
-        self.sprite = pyglet.sprite.Sprite(met, batch=batch)
+        vec = pyglet.image.load(imgpng)
+        vec.anchor_x = vec.width // 2
+        vec.anchor_y = vec.height // 2
+
+        self.sprite = pyglet.sprite.Sprite(vec, batch=batch)
         self.sprite.x = mimox
         self.sprite.y = mimoy
+        self.sprite.acc = acc
+        self.sprite.rot = rot
+        self.sprite.ziv = ziv
 
 
-    def pohyb_meteoru(self,t):
+class Meteor(Object):
 
-        if len(meteory_2)==6:
+    def pohyb_meteoru(self, t, player, laser, skup, predskup, cilskup, met):
 
-            self.sprite.x = mimox
-            self.sprite.y = mimoy
-
-        if len(meteory_4)==6:
-
-            meteory.discard(self)
-            meteory_3.discard(self)
-            meteory_2.clear()
-            meteory_4.clear()
-
-        if self not in meteory and self not in meteory_3:
+        if self.sprite.acc == METEORB_ACCELERATION and self not in skup and self not in cilskup:
 
             self.sprite.x = random.uniform(20, WIDTH-20)
             self.sprite.y = random.choice(pozice)
             self.sprite.rotation = random.uniform(0, 360)
-            meteory.add(self)
+            skup.append(self)
 
-        if self in meteory and self not in meteory_3:
-            if self.sprite.x > WIDTH:
-                self.sprite.x = 0
-            elif self.sprite.y < 0 :
-                self.sprite.y = HEIGHT
-            elif self.sprite.x < 0:
-                self.sprite.x = WIDTH
-            elif self.sprite.y > HEIGHT :
-                self.sprite.y = 0
-            else:
-                self.sprite.x = self.sprite.x + t * METEORM_ACCELERATION * math.cos(math.radians(self.sprite.rotation))
-                self.sprite.y = self.sprite.y + t * METEORM_ACCELERATION * math.sin(math.radians(self.sprite.rotation))
-        if abs(Player_laser.sprite.x - self.sprite.x) < 50 and abs(Player_laser.sprite.y - self.sprite.y) < 50:
+        if self.sprite.acc != METEORB_ACCELERATION and self not in skup and self not in cilskup and len(predskup) != 0:
 
-            Player_laser.sprite.x = mimolx
-            Player_laser.sprite.y = mimoly
-            meteory_3.add(self)
-
-        if abs(Player_ship.sprite.x - self.sprite.x) < 60 and abs(Player_ship.sprite.y - self.sprite.y) < 60:
-
-            Player_ship.sprite.x = WIDTH//2
-            Player_ship.sprite.y = HEIGHT//2
-            zivoty.add(self)
-
-            if len(zivoty) > 2:
-                Player_ship.sprite.x = mimolx
-                Player_ship.sprite.y = mimoly
-
-            self.sprite.x = mimox
-            self.sprite.y = mimoy
-
-            meteory_3.add(self)
-
-    def zbytek_meteoru(self,t,met):
-
-        if met in meteory_3 and self not in meteory_2:
-
+            met = predskup.pop()
             self.sprite.x = met.sprite.x
             self.sprite.y = met.sprite.y
+            if len(predskup) == 0:
+                met.sprite.x = mimox
+                met.sprite.y = mimoy
             self.sprite.rotation = random.uniform(0, 360)
-            meteory_2.add(self)
+            skup.append(self)
 
-        if self in meteory_2 and self not in meteory_4:
+        if self.sprite.x != mimox:
             if self.sprite.x > WIDTH:
                 self.sprite.x = 0
             elif self.sprite.y < 0 :
@@ -144,94 +117,74 @@ class Meteor:
             elif self.sprite.y > HEIGHT :
                 self.sprite.y = 0
             else:
-                self.sprite.x = self.sprite.x + t * METEORT_ACCELERATION * math.cos(math.radians(self.sprite.rotation))
-                self.sprite.y = self.sprite.y + t * METEORT_ACCELERATION * math.sin(math.radians(self.sprite.rotation))
+                self.sprite.x = self.sprite.x + t * self.sprite.acc * math.cos(math.radians(self.sprite.rotation))
+                self.sprite.y = self.sprite.y + t * self.sprite.acc * math.sin(math.radians(self.sprite.rotation))
 
-        if abs(Player_laser.sprite.x - self.sprite.x) < 30 and abs(Player_laser.sprite.y - self.sprite.y) < 30:
+        if abs(laser.sprite.x - self.sprite.x) < 50 and abs(laser.sprite.y - self.sprite.y) < 50:
 
-            Player_laser.sprite.x = mimolx
-            Player_laser.sprite.y = mimoly
+            laser.sprite.x = mimolx
+            laser.sprite.y = mimoly
+            cilskup.append(self)
+            cilskup.append(self)
 
-            self.sprite.x = mimox
-            self.sprite.y = mimoy
+        if abs(player.sprite.x - self.sprite.x) < 60 and abs(player.sprite.y - self.sprite.y) < 60:
 
-            meteory_4.add(self)
+            player.sprite.x = WIDTH//2
+            player.sprite.y = HEIGHT//2
+            cilskup.append(self)
+            cilskup.append(self)
+            player.sprite.ziv -= 1
+            if player.sprite.ziv == 1:
+                player.sprite.x = mimolx
+                player.sprite.y = mimoly
 
-        if abs(Player_ship.sprite.x - self.sprite.x) < 30 and abs(Player_ship.sprite.y - self.sprite.y) < 30:
 
-            Player_ship.sprite.x = WIDTH//2
-            Player_ship.sprite.y = HEIGHT//2
-            zivoty.add(self)
 
-            if len(zivoty) > 2:
-                Player_ship.sprite.x = mimolx
-                Player_ship.sprite.y = mimoly
+class Spaceship(Object):
 
-            self.sprite.x = mimox
-            self.sprite.y = mimoy
-
-            meteory_4.add(self)
-
-class Spaceship:
-
-    def __init__(self):
-
-        Ship = pyglet.image.load('playerShip1_red.png')
-        Ship.anchor_x = Ship.width // 2
-        Ship.anchor_y = Ship.height // 2
-        self.sprite = pyglet.sprite.Sprite(Ship, batch=batch)
-        self.sprite.x = WIDTH//2
-        self.sprite.y = HEIGHT//2
-        self.lives = zivoty
-
-    def zivoty(self):
-        self.lives = zivoty
-    def pohyb_lodi(self,t):
-
-        la = 1
+    def pohyb_lodi(self, t, laser):
 
         if 'w' in stisknute_klavesy:
 
-            self.sprite.x = self.sprite.x + t * ACCELERATION * math.cos(math.radians(90-self.sprite.rotation))
-            self.sprite.y = self.sprite.y + t * ACCELERATION * math.sin(math.radians(90-self.sprite.rotation))
+            self.sprite.x = self.sprite.x + t * self.sprite.acc * math.cos(math.radians(90-self.sprite.rotation))
+            self.sprite.y = self.sprite.y + t * self.sprite.acc * math.sin(math.radians(90-self.sprite.rotation))
 
         if 's' in stisknute_klavesy:
 
-            self.sprite.x = self.sprite.x - t * ACCELERATION * math.cos(math.radians(90-self.sprite.rotation))
-            self.sprite.y = self.sprite.y - t * ACCELERATION * math.sin(math.radians(90-self.sprite.rotation))
+            self.sprite.x = self.sprite.x - t * self.sprite.acc * math.cos(math.radians(90-self.sprite.rotation))
+            self.sprite.y = self.sprite.y - t * self.sprite.acc * math.sin(math.radians(90-self.sprite.rotation))
 
         if 'd' in stisknute_klavesy:
 
-            self.sprite.rotation = self.sprite.rotation + t * rotation_speed
+            self.sprite.rotation = self.sprite.rotation + t * self.sprite.rot
 
         if 'a' in stisknute_klavesy:
 
-            self.sprite.rotation = self.sprite.rotation - t * rotation_speed
+            self.sprite.rotation = self.sprite.rotation - t * self.sprite.rot
 
         if 'r' in stisknute_klavesy and 'l' not in stisknute_klavesy:
 
-
-                Player_laser.sprite.x = self.sprite.x
-                Player_laser.sprite.y = self.sprite.y
-                Player_laser.sprite.rotation = self.sprite.rotation
-                stisknute_klavesy.add('l')
-                stisknute_klavesy.discard('r')
+            laser.sprite.x = self.sprite.x
+            laser.sprite.y = self.sprite.y
+            laser.sprite.rotation = self.sprite.rotation
+            stisknute_klavesy.add('l')
+            stisknute_klavesy.discard('r')
 
         if 'l' in stisknute_klavesy:
 
-                rx = t * LASER_ACCELERATION * math.cos(math.radians(90-Player_laser.sprite.rotation))
-                ry = t * LASER_ACCELERATION * math.sin(math.radians(90-Player_laser.sprite.rotation))
+            rx = t * LASER_ACCELERATION * math.cos(math.radians(90-Player_laser.sprite.rotation))
+            ry = t * LASER_ACCELERATION * math.sin(math.radians(90-Player_laser.sprite.rotation))
 
-                if Player_laser.sprite.x > WIDTH or Player_laser.sprite.x < 0 or Player_laser.sprite.y > HEIGHT or Player_laser.sprite.y < 0:
+            if laser.sprite.x > WIDTH or laser.sprite.x < 0 or laser.sprite.y > HEIGHT or laser.sprite.y < 0:
 
-                    Player_laser.sprite.x = mimox
-                    Player_laser.sprite.y = mimoy
-                    stisknute_klavesy.discard('l')
+                laser.sprite.x = mimox
+                laser.sprite.y = mimoy
+                stisknute_klavesy.discard('l')
 
-                else:
+            else:
 
-                    Player_laser.sprite.x = Player_laser.sprite.x + rx
-                    Player_laser.sprite.y = Player_laser.sprite.y + ry
+                laser.sprite.x = laser.sprite.x + rx
+                laser.sprite.y = laser.sprite.y + ry
 
 class Wazer:
 
@@ -244,30 +197,39 @@ class Wazer:
         self.sprite.x = mimolx
         self.sprite.y = mimoly
 
+def meteory_skup(met, metb, metm, mets, mett):
+    for a in range(pocet_meteoru):
+        met = Meteor(Met, random.choice(meteory_big), METEORB_ACCELERATION, ROTATION_SPEED, 1)
+        metb.append(met)
+    for a in range(pocet_meteoru*2):
+        met = Meteor(Met, random.choice(meteory_med), METEORM_ACCELERATION, ROTATION_SPEED, 1)
+        metm.append(met)
+    for a in range(pocet_meteoru*4):
+        met = Meteor(Met, random.choice(meteory_small), METEORS_ACCELERATION, ROTATION_SPEED, 1)
+        mets.append(meteory_sjmena[pocet_meteoru-1])
+    for a in range(pocet_meteoru*8):
+        met = Meteor(Met, random.choice(meteory_tiny), METEORT_ACCELERATION, ROTATION_SPEED, 1)
+        mett.append(met)
 
-Player_ship = Spaceship()
+meteory_skup(Met, meteory_b, meteory_m, meteory_s, meteory_t)
+
+#self, vec, imgpng, acc, rot, ziv
+Player_ship = Spaceship(Met, 'playerShip1_red.png', ACCELERATION, ROTATION_SPEED, 5)
+Player_ship.sprite.x = WIDTH//2
+Player_ship.sprite.y = HEIGHT//2
 Player_laser = Wazer()
 
-metb1 = Meteor(Metb1)
-metb1.sprite.x = mimox
-metb1.sprite.y = mimoy
-metm1 = Meteor(Metm1)
-
-metm2 = Meteor(Metm2)
-mets1 = Meteor(Mets1)
-mets2 = Meteor(Mets2)
-mett1 = Meteor(Mett1)
-mett2 = Meteor(Mett2)
-
 def cas(t):
-    Player_ship.pohyb_lodi(t)
-    metb1.pohyb_meteoru(t)
-    metm1.zbytek_meteoru(t, metb1)
-    metm2.zbytek_meteoru(t, metb1)
-    mets1.zbytek_meteoru(t, metb1)
-    mets2.zbytek_meteoru(t, metb1)
-    mett1.zbytek_meteoru(t, metb1)
-    mett2.zbytek_meteoru(t, metb1)
+    Player_ship.pohyb_lodi(t, Player_laser)
+    #self, t, player, laser, skup, predskup, cilskup, met
+    for e in range(pocet_meteoru):
+        meteory_b[pocet_meteoru-1].pohyb_meteoru(t, Player_ship, Player_laser, meteory_b3, meteory_t2, meteory_b2, Met)
+    for e in range(pocet_meteoru*2):
+        meteory_m[pocet_meteoru-1].pohyb_meteoru(t, Player_ship, Player_laser, meteory_m3, meteory_b2, meteory_m2, Met)
+    for e in range(pocet_meteoru*4):
+        meteory_s[pocet_meteoru-1].pohyb_meteoru(t, Player_ship, Player_laser, meteory_s3, meteory_m2, meteory_s2, Met)
+    for e in range(pocet_meteoru*8):
+        meteory_t[pocet_meteoru-1].pohyb_meteoru(t, Player_ship, Player_laser, meteory_t3, meteory_s2, meteory_t2, Met)
 
 def stisk_klavesy(symbol,m):
 
